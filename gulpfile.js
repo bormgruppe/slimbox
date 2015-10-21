1dfa57bae9555998cb1de4dc1fff4ef41ba4bc69
Nicholas.Pleschko <nicholas.pleschko@cubx-software.com>
2015-10-21 11:43:16 +0200

    BORM-9921 Fixing serve example task to work for all hostnames
    +review BR-8705

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var del = require('del');
 var runSequence = require('run-sequence');
-var webserver = require('gulp-webserver');
 var concat = require('gulp-concat');
+
+// used to serve the example
+var connect = require('connect');
+var static = require('serve-static');
+var open = require('open');
 
 gulp.task('clean', function(){
     return del.sync('./dist/**');
@@ -29,10 +34,19 @@ gulp.task('rebuild-all', function(){
     runSequence('clean', 'concat-js');
 });
 
+// Serve dir at port and open the browser at path (relative to dir)
+var serveAndOpen = function(dir, port, path) {
+    connect()
+        .use(static(dir))
+        .listen(port, function() {
+            gutil.log('Starting webserver on port ' + port);
+            var openPath = 'http://localhost:' + port + '/' + path;
+            gutil.log('Opening browser at ' + openPath);
+            open(openPath);
+        });
+};
+
 gulp.task('serve-example', function(){
-    gulp.src('./')
-        .pipe(webserver({
-            open: 'example.html'
-        }));
+    serveAndOpen('./', 8000, 'example.html');
 });
 
